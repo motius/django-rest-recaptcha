0cc0be912ee18e95a1b23462f62749011160275b
Sagar Ramachandrappa <sagar.ramachandrappa@motius.de>
2018-08-15 22:23:23 +0200

    Update version and license

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-recaptcha',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',
+    license='MIT License',
     description='Django rest recaptcha field for easy integration of google recaptcha with django-rest-framework.',
     long_description=README,
     url='https://github.com/motius/django-rest-recaptcha',
@@ -26,7 +26,7 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.8',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
