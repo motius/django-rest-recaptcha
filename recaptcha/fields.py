c800ce2d85134bf5e5b97eb717bc8b31cf1241eb
Fox Danger Piacenti <fox@opencraft.com>
2023-01-04 11:13:59 -0600

    fix: replace ugettext_lazy with gettext_lazy for Django 4.X compatibility

--- recaptcha/fields.py
+++ recaptcha/fields.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
